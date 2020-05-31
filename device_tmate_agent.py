a27ce98e9f665abe10edf6f5c34c142f8348e907
Konstantinos Panayiotou <klpanagi@gmail.com>
2020-06-01 00:53:52 +0300

    .

--- device_tmate_agent.py
+++ device_tmate_agent.py
@@ -10,6 +10,23 @@ from amqp_common import (
 )
 
 
+class AgentConfig():
+    """TODO"""
+    # __slots__ = []
+    def __init__(self, broker_username, broker_password, broker_host,
+                 broker_port, broker_vhost, broker_event_exchange,
+                 tmate_socket_path):
+        self.broker_params = {
+            'username': broker_username,
+            'password': broker_password,
+            'host': broker_host,
+            'port': broker_port,
+            'vhost': broker_vhost,
+            'event_exchange': broker_event_exchange
+        }
+        self.tmate_socket_path = tmate_socket_path
+
+
 class DeviceTmateAgent():
     """Device tmate agent class.
     Implementation of an AMQP agent that handles ssh tunelling via a tmate
@@ -41,6 +58,7 @@ class DeviceTmateAgent():
             'x', self.username)
         self.debug = True
         self.hz = 1
+        self.publish_tunnel_info = False
         con_params = ConnectionParameters(host=self.host, port=self.port,
                                           vhost=self.vhost)
         con_params.credentials = Credentials(self.username, self.password)
@@ -121,18 +139,55 @@ class DeviceTmateAgent():
         }
         return _info
 
+    def _tmate_get_ssh_info(self):
+        out = subprocess.check_output(['tmate',  '-S', '/tmp/tmate.sock',
+                                       'display', '-p', '\'#{tmate_ssh}\''],
+                                      stderr=subprocess.STDOUT)
+        return out.decode('utf8').split('\n')[0]
+
+    def _tmate_get_ssh_ro_info(self):
+        out = subprocess.check_output(['tmate',  '-S', '/tmp/tmate.sock',
+                                       'display', '-p', '\'#{tmate_ssh_ro}\''],
+                                      stderr=subprocess.STDOUT)
+        return out.decode('utf8').split('\n')[0]
+
+    def _launch_tmate_client_headless(self):
+        try:
+            out = subprocess.check_output(['tmate',  '-S', self.tmate_sock_path,
+                                           'new-session', '-d'],
+                                          stderr=subprocess.STDOUT)
+            out = subprocess.check_output(['tmate',  '-S', self.tmate_sock_path,
+                                           'wait', 'tmate-ready'],
+                                          stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as exc:
+            print('[ERROR]: {} - {}'.format(exc.returncode, exc.output))
+            return False
+        print('[INFO]: Tmate client Connected!')
+        return True
+
     def run(self):
         rate = Rate(self.hz)
         self.start_tmate_client()
         while True:
             # Publish once
             self.hb_em.send_event(self.hb_event)
-            self._publish_tunnel_info()
+            if self.publish_tunnel_info:
+                self._publish_tunnel_info()
             rate.sleep()
 
     def start_tmate_client(self):
-        out = self.spawn_tmate_client()
-        self.parse_output(out)
+        if os.path.exists(self.tmate_sock_path):
+            self.stop_tmate_agent()
+        _status = self._launch_tmate_client_headless()
+        if not _status:
+            return
+        _out = self._tmate_get_ssh_info()
+        self.ssh_con_str = _out
+        _out = self._tmate_get_ssh_ro_info()
+        self.ssh_con_ro_str = _out
+        print('[INFO]: Tunnel info: {}'.format(self.ssh_con_str))
+        print('[INFO]: Read-Only Tunnel info: {}'.format(
+            self.ssh_con_str))
 
     def spawn_tmate_client(self):
         out = subprocess.check_output(['bash', 'tmate_connect.sh'],
@@ -140,9 +195,12 @@ class DeviceTmateAgent():
         return out
 
     def stop_tmate_agent(self):
-        os.remove(self.tmate_sock_path)
+        if os.path.exists(self.tmate_sock_path):
+            os.remove(self.tmate_sock_path)
+        self.ssh_con_str = ''
+        self.ssh_con_ro_str = ''
 
-    def parse_output(self, out):
+    def parse_sh_script_output(self, out):
         out = out.decode('utf8').split('\n')
         if 'create session failed' in out[0]:
             print('[WARN] - {}'.format(out[0]))
