cad6fe1a5110efae9fb54b5db879d5def7521268
Konstantinos Panayiotou <klpanagi@gmail.com>
2020-06-03 02:58:47 +0300

    Agent configuration basics

--- device_tmate_agent.py
+++ device_tmate_agent.py
@@ -11,21 +11,39 @@ from amqp_common import (
 
 
 class AgentConfig():
-    """TODO"""
+    """DeviceTmateAgent configuration class.
+
+    Args:
+    """
     # __slots__ = []
-    def __init__(self, broker_username, broker_password, broker_host,
-                 broker_port, broker_vhost, broker_event_exchange,
-                 tmate_socket_path):
+    def __init__(self, broker_username='guest', broker_password='guest',
+                 broker_host='localhost', broker_port=5782, broker_vhost='/',
+                 broker_event_exchange='amq.topic', broker_rpc_exchange='',
+                 tmate_socket_path='/tmp/tmate.sock',
+                 hb_event_name='thing.{DEVICE_ID}.tmateagent.heartbeat',
+                 start_rpc_name='thing.{DEVICE_ID}.tmateagent.start',
+                 stop_rpc_name='thing.{DEVICE_ID}.tmateagent.stop',
+                 tunnel_info_rpc_name='thing.{DEVICE_ID}.tmateagent.tunnel_info',
+                 heartbeat_interval=2, device_id=None):
         self.broker_params = {
             'username': broker_username,
             'password': broker_password,
             'host': broker_host,
             'port': broker_port,
             'vhost': broker_vhost,
-            'event_exchange': broker_event_exchange
+            'event_exchange': broker_event_exchange,
+            'rpc_exchange': broker_rpc_exchange
         }
         self.tmate_socket_path = tmate_socket_path
-
+        self.heartbeat_interval = heartbeat_interval
+        if device_id is None:
+            device_id = broker_username
+        self.device_id = device_id
+        self.hb_event_name = hb_event_name.replace('{DEVICE_ID}', device_id)
+        self.start_rpc_name = start_rpc_name.replace('{DEVICE_ID}', device_id)
+        self.stop_rpc_name = stop_rpc_name.replace('{DEVICE_ID}', device_id)
+        self.tunnel_info_rpc_name = tunnel_info_rpc_name.replace(
+            '{DEVICE_ID}', device_id)
 
 class DeviceTmateAgent():
     """Device tmate agent class.
@@ -35,34 +53,29 @@ class DeviceTmateAgent():
 
     Args:
     """
-    def __init__(self):
+    def __init__(self, config=None):
         self.ssh_con_str = ''
         self.ssh_con_ro_str = ''
 
-        self.tmate_sock_path = '/tmp/tmate.sock'
+        if config is None:
+            config = AgentConfig(broker_host='r4a-platform.ddns.net',
+                                 broker_username='device0',
+                                 broker_password='k!sh@')
+        self.config = config
 
-        self.username = 'device0'
-        self.password = 'device0'
-        self.host = 'r4a-platform.ddns.net'
-        self.port = 5782
-        self.vhost = '/'
-        self.hb_event_id = 'thing.{}.tmateagent.heartbeat'.format(
-            self.username)
-        self.start_rpc_name = 'thing.x.tmateagent.start'.replace(
-            'x', self.username)
-        self.stop_rpc_name = 'thing.x.tmateagent.stop'.replace(
-            'x', self.username)
-        self.tunnel_info_rpc_name = 'thing.x.tmateagent.tunnel_info'.replace(
-            'x', self.username)
-        self.tunnel_info_topic = 'thing.x.tmateagent.tunnel_info'.replace(
-            'x', self.username)
         self.debug = True
-        self.hz = 1
-        self.publish_tunnel_info = False
-        con_params = ConnectionParameters(host=self.host, port=self.port,
-                                          vhost=self.vhost)
-        con_params.credentials = Credentials(self.username, self.password)
-        options = EventEmitterOptions(exchange='amq.topic')
+
+        self._init_broker_endpoints()
+
+    def _init_broker_endpoints(self):
+        b_params = self.config.broker_params
+        con_params = ConnectionParameters(
+            host=b_params['host'], port=b_params['port'],
+            vhost=b_params['vhost']
+        )
+        con_params.credentials = Credentials(b_params['username'],
+                                             b_params['password'])
+        options = EventEmitterOptions(exchange=b_params['event_exchange'])
 
         self.hb_em = EventEmitter(
             options,
@@ -70,34 +83,29 @@ class DeviceTmateAgent():
             debug=self.debug
         )
 
-        self.hb_event = Event(name=self.hb_event_id, payload={}, headers={})
+        self.hb_event = Event(name=self.config.hb_event_name,
+                              payload={}, headers={})
 
         self.start_rpc = RpcServer(
-            self.start_rpc_name,
+            self.config.start_rpc_name,
             on_request=self._start_rpc_callback,
             connection_params=con_params,
             debug=self.debug)
         self.start_rpc.run_threaded()
 
         self.stop_rpc = RpcServer(
-            self.stop_rpc_name,
+            self.config.stop_rpc_name,
             on_request=self._stop_rpc_callback,
             connection_params=con_params,
             debug=self.debug)
         self.stop_rpc.run_threaded()
 
         self.tunnel_info_rpc = RpcServer(
-            self.tunnel_info_rpc_name,
+            self.config.tunnel_info_rpc_name,
             on_request=self._tunnel_info_rpc_callback,
             connection_params=con_params,
             debug=self.debug)
         self.tunnel_info_rpc.run_threaded()
-
-        self.pub = PublisherSync(
-            self.tunnel_info_topic,
-            connection_params=con_params,
-            debug=self.debug
-        )
 
     def _start_rpc_callback(self, msg, meta):
         print('[DEBUG] - Start RPC Call')
@@ -153,10 +161,10 @@ class DeviceTmateAgent():
 
     def _launch_tmate_client_headless(self):
         try:
-            out = subprocess.check_output(['tmate',  '-S', self.tmate_sock_path,
+            out = subprocess.check_output(['tmate',  '-S', self.config.tmate_socket_path,
                                            'new-session', '-d'],
                                           stderr=subprocess.STDOUT)
-            out = subprocess.check_output(['tmate',  '-S', self.tmate_sock_path,
+            out = subprocess.check_output(['tmate',  '-S', self.config.tmate_socket_path,
                                            'wait', 'tmate-ready'],
                                           stderr=subprocess.STDOUT)
         except subprocess.CalledProcessError as exc:
@@ -166,17 +174,19 @@ class DeviceTmateAgent():
         return True
 
     def run(self):
-        rate = Rate(self.hz)
+        rate = Rate(1 / self.config.heartbeat_interval)
         self.start_tmate_client()
         while True:
             # Publish once
             self.hb_em.send_event(self.hb_event)
-            if self.publish_tunnel_info:
-                self._publish_tunnel_info()
             rate.sleep()
 
     def start_tmate_client(self):
-        if os.path.exists(self.tmate_sock_path):
+        """Start tmate client process.
+        The process is spawned in daemon mode and tunnel info are printed
+        upon successfull connection with the remote tmate server.
+        """
+        if os.path.exists(self.config.tmate_socket_path):
             self.stop_tmate_agent()
         _status = self._launch_tmate_client_headless()
         if not _status:
@@ -189,14 +199,18 @@ class DeviceTmateAgent():
         print('[INFO]: Read-Only Tunnel info: {}'.format(
             self.ssh_con_str))
 
-    def spawn_tmate_client(self):
+    def spawn_tmate_client_sh(self):
+        """Execute tmate_connect.sh bash script in fork mode."""
         out = subprocess.check_output(['bash', 'tmate_connect.sh'],
                                       stderr=subprocess.STDOUT)
         return out
 
     def stop_tmate_agent(self):
-        if os.path.exists(self.tmate_sock_path):
-            os.remove(self.tmate_sock_path)
+        """Stops tmate client gracefully.
+        Removes local tmate socket.
+        """
+        if os.path.exists(self.config.tmate_socket_path):
+            os.remove(self.config.tmate_socket_path)
         self.ssh_con_str = ''
         self.ssh_con_ro_str = ''
 
